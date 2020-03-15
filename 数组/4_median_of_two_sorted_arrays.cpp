c93edecd1785397466457508477e01cf5f86937c
ZackQ <18207143503@163.com>
2020-03-15 12:29:48 +0800

    二分查找中位数的实现

--- 数组/4_median_of_two_sorted_arrays.cpp
+++ 数组/4_median_of_two_sorted_arrays.cpp
@@ -17,7 +17,54 @@ using namespace std;
  *      若nums1[k/2]<nums2[k/2]，同理，nums1的前k/2个元素一定在整体的topK元素内，因此从剩下的元素中，找到整体的第k/2个元素
  * 还要注意的是当元素个数为偶数时，取中间两个元素的平均数
 */
+#define BINARY_SEARCH
+#ifdef BINARY_SEARCH
+// 二分查找实现算法
+class Solution {
+public:
+	double findMedianSortedArrays(vector<int> &nums1, vector<int> &nums2);
+	// 找到在两个数组中第K小的数字
+	int findKthInSortedArrays(vector<int> &nums1, int beg1, int m, vector<int> &nums2, int beg2, int n, int k);
+};
 
+int Solution::findKthInSortedArrays(vector<int> &nums1, int beg1, int m, vector<int> &nums2, int beg2, int n, int k) {
+	// 从两个数组中一共取出k个元素，按照每个数组取出前k/2个元素，这样可以保证log的时间复杂度(因为用的是二分的方法取)
+	// 但可能元素较少的数组中不够k/2个元素，因此需要判断一下能否从较小的数组中取出k/2个，不能的话可以直接将全部元素
+	// 取出，并且在另一个数组中取出前k-m个即可，这样保证取出的元素个数一共仍是k个
+	// 当较少个数元素的数组为空时，直接取出另一个数组的首个元素(一定存在，可以用反证法说明)
+	// 同时注意边界情况，当k为1时，意思是取出一个元素，可以直接判断
+	if (m > n) {
+		return findKthInSortedArrays(nums2, beg2, n, nums1, beg1, m, k);
+	}
+	if (m == 0) {
+		assert(k <= n);
+		return nums2[k - 1];
+	}
+	if (k == 1) {
+		return min(nums1[beg1], nums2[beg2]);
+	}
+	// 分别计算从nums1和nums2中取出多少个元素
+	int fetchLenOfA = min(k/2, m);
+	int fetchLenOfB = k - fetchLenOfA;
+	if (nums1[beg1 + fetchLenOfA - 1] == nums2[beg2 + fetchLenOfB - 1])
+		return nums1[beg1 + fetchLenOfA - 1];
+	else if (nums1[beg1 + fetchLenOfA - 1] > nums2[beg2 + fetchLenOfB - 1])
+		return findKthInSortedArrays(nums1, beg1, m, nums2, beg2 + fetchLenOfB, n - fetchLenOfB, k - fetchLenOfB);
+	else
+		return findKthInSortedArrays(nums1, beg1 + fetchLenOfA, m - fetchLenOfA, nums2, beg2, n, k - fetchLenOfA);
+}
+
+double Solution::findMedianSortedArrays(vector<int> &nums1, vector<int> &nums2) {
+	if ((nums1.size() + nums2.size()) & 0x1) {
+		return static_cast<double>(findKthInSortedArrays(nums1, 0, nums1.size(), nums2, 0, nums2.size(), (nums1.size() + nums2.size())/2 + 1));
+	} else {
+		int a = findKthInSortedArrays(nums1, 0, nums1.size(), nums2, 0, nums2.size(), (nums1.size() + nums2.size())/2);
+		int b = findKthInSortedArrays(nums1, 0, nums1.size(), nums2, 0, nums2.size(), (nums1.size() + nums2.size())/2 + 1);
+		cout << "Two var: " << a << ' ' << b << endl;
+		return (a + b) / 2.0;
+	}
+}
+#else
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>  &nums1, vector<int> &nums2);
@@ -56,10 +103,11 @@ double Solution::findMedianSortedArrays(vector<int> &nums1, vector<int> &nums2) 
 	// cout << "Two vars : " << var1 << ' ' << var2 << endl;
 	return (var1 + var2) / 2.0;
 }
+#endif
 
 int main(int argc, char *argv[]) {
-	vector<int> nums1 = {1,2};
-	vector<int> nums2 = {3,4};
+	vector<int> nums1 = {1};
+	vector<int> nums2 = {1};
 	Solution solution;
 	cout << "The median is " << solution.findMedianSortedArrays(nums1, nums2) << endl;
 	return 0;
